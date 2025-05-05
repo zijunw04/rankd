1132df615849cf354068dbc9da488ef023dda0ac
zijunw04 <floatsinkfc@gmail.com>
2025-05-04 22:07:44 -0500

    analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/react"
 
 
 const geistSans = Geist({
@@ -26,6 +27,7 @@ export default function RootLayout({ children }) {
         className={`${geistSans.variable} ${geistMono.variable} antialiased bg-white`}
       >
         {children}
+        <Analytics />
       </body>
     </html>
     </>
